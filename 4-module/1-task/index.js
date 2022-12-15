159d635acb4e52093be0de40b848d96a3e93bda7
marina210 <BalmakovaMV@alrosa.ru>
2022-12-15 22:48:20 +0900

    Модуль 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
 function makeFriendsList(friends) {
-  // ваш код...
+    
+let list = '\n'; //'<ul> \n';
+   for (friend of friends) {
+     list = list + ('  <li>' + friend.firstName + ' ' + friend.lastName + '</li>\n');
+   };
+  // list = list +'</ul>';
+  let ul = document.createElement('ul');
+   ul.innerHTML= list;
+//document.body.append(ul);
+  
+return ul;
+  
+
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,49 @@
 function highlight(table) {
-  // ваш код...
+//const table = document.body.querySelector('table');
+ 
+//console.log( table.rows[0].cells[0].innerHTML );
+ 
+let colStatus = 0;
+ let colGender =0;
+ let colAge = 0;
+for (let i=0; i< table.rows[0].cells.length; i++) {
+ switch(table.rows[0].cells[i].textContent) {
+  case 'Status':  
+    colStatus = i;
+    break;
+  case 'Gender':  
+    colGender = i;
+    break;  
+  case 'Age':  
+    colAge = i;
+   };
+ 
 }
+ // console.log(colStatus,colGender,colAge);
+for (let i=1; i< table.rows.length; i++ ){
+ //console.log(table.rows[i].cells[colStatus].dataset.available);
+  
+ switch (table.rows[i].cells[colStatus].dataset.available ) {
+   case 'true': 
+     table.rows[i].className ='available' ;
+     break;
+   case 'false': 
+     table.rows[i].className ='unavailable' ;
+     break;
+   case undefined :
+     table.rows[i].style.hidden = 'true' ;
+ };
+  
+if (table.rows[i].cells[colGender].textContent ==='m') {
+  table.rows[i].className=  'male'; 
+} else if (table.rows[i].cells[colGender].textContent ==='f') {
+  table.rows[i].className ='female';
+} ;
+
+//console.log(parseInt(table.rows[i].cells[colAge].textContent,10));  
+ if ((parseInt(table.rows[i].cells[colAge].textContent,10) || 0) < 18) {
+  table.rows[i].style="text-decoration: line-through"; 
+}
+ //table.rows[i].cells[colStatus].style.backgroundColor = 'red';
+}
+} 
